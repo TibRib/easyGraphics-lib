6bcd76e45f4b04dc8d0fda64c1bded6311d75376
TibRib <meta-block@hotmail.fr>
2020-10-09 11:50:36 +0200

    corrected init

--- easyGraphics/Texture.h
+++
@@ -1,8 +0,0 @@
-#include <GL/freeglut.h>
-#include <SOIL/SOIL.h>
-#include <stdbool.h>
-
-GLuint TexLoad(const char* path);
-void TexBind(GLuint tex, bool filtered);
-bool checkLoaded(GLuint *myTexture);
-void TexEnd(void);
